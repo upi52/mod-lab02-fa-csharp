7805870e1f2e621a2ab25fdfda7a1d1124ccb4cf
upi52 <popkadiler@gmail.com>
2026-04-02 19:39:14 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -16,25 +16,112 @@ namespace fans
 
   public class FA1
   {
+    private State q0, q1, q2, q3, qDead;
+    private State InitialState;
+
+    public FA1()
+    {
+      q0    = new State { Name = "q0",    IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q1    = new State { Name = "q1",    IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q2    = new State { Name = "q2",    IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q3    = new State { Name = "q3",    IsAcceptState = true,  Transitions = new Dictionary<char, State>() };
+      qDead = new State { Name = "qDead", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+
+      q0.Transitions['0'] = q2;
+      q0.Transitions['1'] = q1;
+      q1.Transitions['0'] = q3;
+      q1.Transitions['1'] = q1;
+      q2.Transitions['0'] = qDead;
+      q2.Transitions['1'] = q3;
+      q3.Transitions['0'] = qDead;
+      q3.Transitions['1'] = q3;
+      qDead.Transitions['0'] = qDead;
+      qDead.Transitions['1'] = qDead;
+
+      InitialState = q0;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        if (!current.Transitions.ContainsKey(c))
+          return null;
+        current = current.Transitions[c];
+      }
+      return current.IsAcceptState;
     }
   }
 
   public class FA2
   {
+    private State q00, q10, q01, q11;
+    private State InitialState;
+
+    public FA2()
+    {
+      q00 = new State { Name = "q00", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q10 = new State { Name = "q10", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q01 = new State { Name = "q01", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q11 = new State { Name = "q11", IsAcceptState = true,  Transitions = new Dictionary<char, State>() };
+
+      q00.Transitions['0'] = q10;
+      q00.Transitions['1'] = q01;
+      q10.Transitions['0'] = q00;
+      q10.Transitions['1'] = q11;
+      q01.Transitions['0'] = q11;
+      q01.Transitions['1'] = q00;
+      q11.Transitions['0'] = q01;
+      q11.Transitions['1'] = q10;
+
+      InitialState = q00;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        if (!current.Transitions.ContainsKey(c))
+          return null;
+        current = current.Transitions[c];
+      }
+      return current.IsAcceptState;
     }
   }
-  
+
   public class FA3
   {
+    private State q0, q1, q2;
+    private State InitialState;
+
+    public FA3()
+    {
+      q0 = new State { Name = "q0", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q1 = new State { Name = "q1", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+      q2 = new State { Name = "q2", IsAcceptState = true,  Transitions = new Dictionary<char, State>() };
+
+      q0.Transitions['0'] = q0;
+      q0.Transitions['1'] = q1;
+      q1.Transitions['0'] = q0;
+      q1.Transitions['1'] = q2;
+      q2.Transitions['0'] = q2;
+      q2.Transitions['1'] = q2;
+
+      InitialState = q0;
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = InitialState;
+      foreach (var c in s)
+      {
+        if (!current.Transitions.ContainsKey(c))
+          return null;
+        current = current.Transitions[c];
+      }
+      return current.IsAcceptState;
     }
   }
 
